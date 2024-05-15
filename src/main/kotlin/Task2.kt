bb279c8f13731c48beb95220771c14b7d424fa43
lexxv <mister.alex49@yandex.ru>
2024-05-15 14:45:16 +0300

    LR5 TASK2 Реализована функция поиска суммы цифр числа делящихся на 3 и написаны юнит тесты

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -12,4 +12,17 @@ class Task2 {
      * */
     fun maxDigit(number: Int): Int =
         number.absoluteValue.toString().map { it.toString().toInt() }.maxOrNull()!!
+
+    /**
+     * Функция поиска суммы цифр числа делящихся на 3
+     *
+     * @param number {Int} - Число
+     *
+     * @author A.Vorobyev <mister.alex49@yandex.ru>
+     * */
+    fun sumDigitsDividedBy3(number: Int): Int =
+        number.absoluteValue.toString()
+            .map { it.toString().toInt() }
+            .stream().filter { it % 3 == 0 }
+            .toList().fold(0) { acc, it -> acc + it }
 }
--- src/test/kotlin/Task2Test.kt
+++ src/test/kotlin/Task2Test.kt
@@ -5,12 +5,24 @@ import ru.lexxv.university.Task2
 class Task2Test {
     @Test
     fun `maxDigit test`() {
-        val main = Task2()
-        assertEquals(4, main.maxDigit(1234))
-        assertEquals(2, main.maxDigit(1111211))
-        assertEquals(1, main.maxDigit(10000))
-        assertEquals(1, main.maxDigit(1))
-        assertEquals(1, main.maxDigit(-1))
-        assertEquals(0, main.maxDigit(0))
+        val func = Task2()::maxDigit
+        assertEquals(4, func(1234))
+        assertEquals(2, func(1111211))
+        assertEquals(1, func(10000))
+        assertEquals(1, func(1))
+        assertEquals(1, func(-1))
+        assertEquals(0, func(0))
+    }
+
+    @Test
+    fun `sumDigitsDividedBy3 test`() {
+        val func = Task2()::sumDigitsDividedBy3
+        assertEquals(3, func(1234))
+        assertEquals(15, func(33333))
+        assertEquals(0, func(10000))
+        assertEquals(0, func(1))
+        assertEquals(0, func(-1))
+        assertEquals(0, func(0))
+        assertEquals(18, func(-3690))
     }
 }
